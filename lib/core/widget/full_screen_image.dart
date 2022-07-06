56da7a24b211f50d7fe0545910a12ad406999545
Ahmed Hamdy Madkour <86315638+Amadkour@users.noreply.github.com>
2022-07-06 03:09:14 +0200

    save Image [6/7/2022]

--- lib/core/widget/full_screen_image.dart
+++ lib/core/widget/full_screen_image.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_task/core/widget/my_image.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+
+import '../constant/color.dart';
 
 class FullScreenImage extends StatelessWidget {
   const FullScreenImage({Key? key, required this.url, required this.height, required this.width}) : super(key: key);
@@ -10,6 +14,29 @@ class FullScreenImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.2,
+          automaticallyImplyLeading: true,
+          leading: BackButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: AppColors.blackColor,
+          ),
+          actions: [
+            IconButton(onPressed: (){
+              GallerySaver.saveImage(url).then((value) => print(value));
+  }, icon: const Icon(Icons.save_alt))
+          ],
+          // title: Text(
+          //   person.name!,
+          //   style: const TextStyle(
+          //       color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+          // ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
+
       body: MyImage(
         url: url,
         height: height,
